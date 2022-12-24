43aae19b7f79273d6a5f9bf203c800d53adfb7c3
shichangzhen <yuemeng200@outlook.com>
2022-12-24 22:20:50 +0800

    连接neo4j

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { UnprocessibleEntityValidationPipe } from './pipes/unprocessible-entity-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes( new UnprocessibleEntityValidationPipe() );
   app.enableCors()
+  
   await app.listen(3000);
 }
 bootstrap();
